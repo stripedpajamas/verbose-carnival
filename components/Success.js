21779718af03aae29bd7a175eaf1d67bdea11a93
rv-korlando <korlando@redventures.com>
2018-02-10 10:40:26 -0500

    adding logo and message to section

--- components/Success.js
+++ components/Success.js
@@ -5,5 +5,7 @@ export default (props) => (
     <h1>Success!</h1>
     <h2>{props.name}, thank you for applying for the {props.card.name} card</h2>
     <p>You should receive a confirmation email with details on your new card and when it will arrive</p>
+    <img src="static/logo.png" />
+    <p class="description">Thank you for choosing Cards Online to fit your credit card search needs</p>
   </div>
 )
